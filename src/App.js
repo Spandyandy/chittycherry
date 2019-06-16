fe051195ab30ed5ed80394dd9994ec32a5c509a3
Andy Kim <Junghoo.Kim1@ibm.com>
2019-06-16 18:43:25 -0400

    motsaengyotu

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+
+      <div class="d-md-flex h-md-100 align-items-center">
+
+
+        <div class="col-md-6 p-0 bg-indigo h-md-100">
+            <div class="text-white d-md-flex align-items-center h-100 p-5 text-center justify-content-center">
+                <div class="logoarea pt-5 pb-5">
+                    <h1>Chitty Cherry</h1>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-6 p-0 bg-white h-md-100 loginarea">
+            <div class="d-md-flex align-items-center h-md-100 p-5 justify-content-center">
+                <form>
+                  <input type="text" id="login" class="fadeIn second" name="login" placeholder="invitation Code" />
+                  <input type="text" id="password" class="fadeIn third" name="login" placeholder="Name" />
+                  <input type="submit" class="fadeIn fourth" value="Join" />
+                </form>
+            </div>
+        </div>
+            
+        </div>
     </div>
   );
 }
