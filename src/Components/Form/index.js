a97acc0ad36db129a30d11701876fe536d2f26b8
Charlotte Chung <chungsae@chungsae-macbookpro.roam.corp.google.com>
2019-06-18 00:09:47 -0400

    Make fields in log-in form required

--- src/Components/Form/index.js
+++ src/Components/Form/index.js
@@ -1,23 +1,54 @@
 import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button';
 import './index.css';
 
 class Example extends Component {
+	constructor(...args) {
+	    super(...args);
+	    this.state = { validated: false };
+	    this.routeChange = this.routeChange.bind(this);
+	}
+
+	routeChange() {
+		let path = "/chat";
+		window.open(path);
+	}
+
+	handleSubmit(event) {
+	    const form = event.currentTarget;
+	    if (form.checkValidity() === false) {
+	      event.preventDefault();
+	      event.stopPropagation();
+	      this.setState({ validated: true });
+	      return false;
+	    }
+
+	    this.routeChange();
+  	}
+
 	render() {
+		const { validated } = this.state;
+
 		return (
 			<div className="card maincard mx-auto">
-				<Form className="card-body">
+				<Form noValidate
+        			  validated={validated}
+        			  onSubmit={e => this.handleSubmit(e)} 
+        			  className="card-body">
 					<Form.Group controlId="formBasicEmail">
-						<Form.Control type="invcode" placeholder="Invitation Code" />
+						<Form.Control required type="invcode" placeholder="Invitation Code" />
 					</Form.Group>
 					<Form.Group controlId="formBasicPassword">
-						<Form.Control type="name" placeholder="First Name" />
+						<Form.Control required type="name" placeholder="First Name" />
 					</Form.Group>
 					<Form.Group controlId="formBasicPassword">
-					<Form.Control type="name" placeholder="Last Name" />
+					<Form.Control required type="name" placeholder="Last Name" />
 					</Form.Group>
-					<Button className="container-fluid" variant="dark" href="/chat" /*type="submit"*/>Join</Button>
+					<Button type="submit" className="container-fluid" variant="dark" >
+						Join
+					</Button>
 				</Form>
 			</div>
 		)
